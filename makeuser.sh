84aa3c0720fc7628e1c75bcb2931f522b84552d3
elgwhoppo <joeman113@gmail.com>
2017-07-24 23:59:30 -0400

    Create makeuser.sh

---
+++ makeuser.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ 
+####
+# This script automatically creates user accounts with random passwords.
+# It also switches to the user and creates an SSH key
+###
+ 
+if [ $# -lt 1 ]; then
+echo "Please supply a user name"
+echo "Example: " $0 "jsmith"
+exit
+fi
+ 
+# Declare local variables, generate random password.
+ 
+newuser=$1
+randompw=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+ 
+# Create new user and assign random password.
+ 
+useradd $newuser -m
+echo $newuser:$randompw | chpasswd
+echo "UserID:" $newuser "has been created with the following password:" $randompw
+
+sudo -u $newuser -H sh -c "cd /home/$newuser; ssh-keygen -f file.rsa -t rsa -N ''"
+echo "UserID:" $newuser "has a new SSH public and private key located at /home/"$newuser
